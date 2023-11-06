3715cdc6c11fe09f2b98c2b139d9ad2db6de610a
TheBotlyNoob <jj@jj.is-a.dev>
2023-11-06 16:20:43 -0500

    Update dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,10 @@ end
 
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin 
+
+# pnpm
+set -gx PNPM_HOME "/home/jj/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
