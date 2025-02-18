fceadc9dcceb3f4088cdf4581434a97d59f0a2c8
TheBotlyNoob <jj@jjay.dev>
2025-02-18 16:03:00 -0500

    stuffs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,25 @@
 set -g fish_greeting
 
 if status is-interactive
-	# starship init fish | source
+    # starship init fish | source
+    #
+    # #  Initialization of global variables
+    set -q LS_AFTER_CD || set -gx LS_AFTER_CD true
+    set -q LS_AFTER_CD_COMMAND || set -gx LS_AFTER_CD_COMMAND "ls -GF"
+
+    # ls automatically when changing directories
+    function on_directory_change --on-variable PWD
+        if not string match -q "*;*" $LS_AFTER_CD_COMMAND
+            eval $LS_AFTER_CD_COMMAND
+        else
+            echo "Error: LS_AFTER_CD_COMMAND contains invalid characters."
+        end
+    end
 end
 
 fish_add_path ~/.local/share/bin
 
-alias ls 'lsd'
+alias ls lsd
 
 abbr l 'ls -l'
 abbr la 'ls -a'
@@ -19,6 +32,8 @@ abbr ... 'cd ../..'
 abbr .3 'cd ../../..'
 abbr .4 'cd ../../../..'
 abbr .5 'cd ../../../../..'
+
+
 
 # Always mkdir a path (this doesn't inhibit functionality to make a single dir)
 abbr mkdir 'mkdir -p'
